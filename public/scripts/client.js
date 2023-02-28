f318f233210ad57a845e94a8e53e0f47ae32f0fa
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-02-28 15:38:04 -0700

    ➕ loadTweets  function to fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,13 @@
  */
 
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+const loadTweets = function () {
+  $.ajax({
+    type: "GET",
+    url: '/tweets',
+    success: renderTweets,
+  });
+}
 
 /**
  * Renders a list of tweets into the tweets secion.
@@ -75,7 +56,7 @@ const createTweetElement = function (tweet) {
 
 $(document).ready(function () {
 
-  renderTweets(data);
+  loadTweets();
   $('section.new-tweet form').on('submit', function (e) {
     e.preventDefault();
     $.ajax({
