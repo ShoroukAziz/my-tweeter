e87b1b4a6b723d9ee4a44077c68f04fc6717c579
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-02 16:01:36 -0700

    ➕create a file for constatnts & add it to index

---
+++ public/scripts/system-constants.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview Conatines all the constant variables of the system for easier mainainance 
+ */
+
+const MAX_TWEET_LENGHT = 140;
+const ERROR_MESSAGE_DISPLAY_TIME = 2500;
+
+const systemMessages = {
+
+  emptyTweetError: 'Your Tweet can\'t be empty!',
+  longTweetError: `Your Tweet can\'t be more than ${MAX_TWEET_LENGHT} characters. Keep it short & sweet!`
+
+}
