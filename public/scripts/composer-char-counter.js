4c2837ce0e8d635be7962698a083668009a84ab5
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-02 16:05:12 -0700

    ➕add comments & refactor to use constants

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,15 @@
-const TWEET_LENGTH = 140;
+/**
+ * @fileoverview Contains js logic for composing a tweet interactively
+ */
 
 $(document).ready(function () {
 
   // Keep track of charachter count
   $('#tweet-text').on("input", function () {
 
-    const currentCounter = TWEET_LENGTH - $(this).val().length;
+    const currentCounter = MAX_TWEET_LENGHT - $(this).val().length;
     const counterNode = $(this).parent().children().find('output')[0];
-    counterNode.innerText = currentCounter
+    counterNode.innerText = currentCounter;
     if (currentCounter < 0) {
       $(counterNode).addClass('count-warning');
     }
@@ -20,8 +22,8 @@ $(document).ready(function () {
   // Automatically grow the text area to fit input
   $('#tweet-text').on("input", function () {
 
-    $(this).css('overflow', 'hidden')
-    $(this).height('10px')
+    $(this).css('overflow', 'hidden');
+    $(this).height('10px');
     $(this).height($(this)[0].scrollHeight + 10 + "px");
   })
 
